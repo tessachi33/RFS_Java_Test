1cfab00a25d2b32466d74e028b4bda7ff0aac8ea
Tessa <Tessachi33@yahoo.com>
2016-01-13 14:48:45 -0800

    Add initial file structure and Dog class

---
+++ src/main/Java/Dog.java
@@ -0,0 +1,8 @@
+// •	Construct a base class that represents a generic animal. This class should contain at least two properties and two methods
+//
+// 	•	Demonstrate the following in a main application method:
+// 	•	Declare and initialize the class
+// 	•	Reassign at least two class variables using class properties
+// 	•	Make two class method calls
+//
+// 	•	Construct a second class that inherits from the previous animal class. This class should contain at least one overridden method or property as well as one property and one method that are not in the parent class. Feel free to modify the parent class if necessary.
